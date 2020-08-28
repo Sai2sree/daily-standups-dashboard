4d9f866e63a7d9233786e72443efedcf7c1a0d85
Sree Sai Chaitanya Nagarajugari <snagaraj@tibco.com>
2020-08-27 19:23:17 -0700

    create card component and use it in dashboard component

---
+++ src/components/card/card.component.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import "./card.styles.scss";
+
+const Card = () => (
+  <div>
+    <h2>Sree</h2>
+    <ul>
+      <li>issue 1</li>
+      <li>issue 2</li>
+    </ul>
+  </div>
+);
+
+export default Card;
--- src/components/dashboard/dashboard.component.jsx
+++ src/components/dashboard/dashboard.component.jsx
@@ -1,12 +1,14 @@
 import React from "react";
 import "./dashboard.styles.scss";
 import { Component } from "react";
+import Card from "../card/card.component";
 
 class Dashboard extends Component {
   render() {
     return (
       <div>
         <h1>Daily Check In Dashboard</h1>
+        <Card />
       </div>
     );
   }
