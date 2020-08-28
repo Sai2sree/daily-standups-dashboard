2ace47b97269c2d80c32d37088a39f98d8d8ec7c
Sree Sai Chaitanya Nagarajugari <snagaraj@tibco.com>
2020-08-27 18:41:42 -0700

    create  dashboard component and use it in app component.

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import React from "react";
 import "./App.css";
+import Dashboard from "./components/dashboard/dashboard.component";
 
 function App() {
   return (
     <div className="App">
-      <h1>Daily standups dashboard</h1>
+      <Dashboard></Dashboard>
     </div>
   );
 }
---
+++ src/components/dashboard/dashboard.component.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import "./dashboard.styles.scss";
+import { Component } from "react";
+
+class Dashboard extends Component {
+  render() {
+    return (
+      <div>
+        <h1>Daily Check In Dashboard</h1>
+      </div>
+    );
+  }
+}
+
+export default Dashboard;
