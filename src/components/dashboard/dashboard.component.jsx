03e9001f355ce4ab6a99a2f48ac16252ffdcf922
Sree Sai Chaitanya Nagarajugari <snagaraj@tibco.com>
2020-08-30 00:53:30 -0700

    Convert  dashboard component from class to function component. Replace ComponentDidMount life cycle hook with UseEffect hook.

--- src/components/dashboard/dashboard.component.jsx
+++ src/components/dashboard/dashboard.component.jsx
@@ -1,28 +1,25 @@
-import React, { Component } from "react";
+import React, { useEffect } from "react";
 import "./dashboard.styles.scss";
 import Card from "../card/card.component";
 import { connect } from "react-redux";
 import { fetchIssuesStartAsync } from "../../redux/issue/issue.action";
 
-class Dashboard extends Component {
-  componentDidMount() {
-    const { fetchIssuesStartAsync } = this.props;
+const Dashboard = ({ fetchIssuesStartAsync, currentIssues }) => {
+  useEffect(() => {
     fetchIssuesStartAsync();
-  }
+  }, [fetchIssuesStartAsync]);
 
-  render() {
-    return (
-      <div className="dashboard">
-        <h1 className="dashboard__title">Daily Check In Dashboard</h1>
-        <ul className="dashboard__cards">
-          {this.props.currentIssues.map((el) => (
-            <Card key={el.id} assignee={el.name} issues={el.issues} />
-          ))}
-        </ul>
-      </div>
-    );
-  }
-}
+  return (
+    <div className="dashboard">
+      <h1 className="dashboard__title">Daily Check In Dashboard</h1>
+      <ul className="dashboard__cards">
+        {currentIssues.map((el) => (
+          <Card key={el.id} assignee={el.name} issues={el.issues} />
+        ))}
+      </ul>
+    </div>
+  );
+};
 
 const mapStateToProps = (state) => ({
   currentIssues: state.issue.currentIssues,
